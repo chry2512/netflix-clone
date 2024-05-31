dd6746f1daa12aaa8950ad2dec33a5e5daf57563
Chris <christianniro18@gmail.com>
2024-05-31 18:24:55 +0200

    v.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Netflix Clone
+
+This is the clone of a popular video streaming website [Netflix](https://netflix.com/browse), for academy project.
---
+++ scripts/nav.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector("nav");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 5) {
+            nav.classList.add("nav-dark");
+        } else {
+            nav.classList.remove("nav-dark");
+        }
+    });
+
+    document.getElementById("playButton").addEventListener("click", function () {
+        window.localStorage.setItem("playEventTriggered", "playlist");
+        window.location.href = "./page/player.html";
+    });
+
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`locandina${i}`).addEventListener("click", function () {
+            window.localStorage.setItem("playEventTriggered", `locandina${i}`);
+            window.location.href = "./page/player.html";
+        });
+    }
+});
+
+
